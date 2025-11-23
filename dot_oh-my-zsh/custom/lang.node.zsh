3edc19b099c8ac0c7acf80aecf0d1240fe22e93b
RyanLincs <ryan.lincs@outlook.com>
2025-11-23 13:18:20 +0800

    添加node

---
+++ dot_oh-my-zsh/custom/lang.node.zsh
@@ -0,0 +1,7 @@
+export LDFLAGS="-L/usr/local/opt/node/lib"
+export CPPFLAGS="-I/usr/local/opt/node/include"
+
+# node@24
+# export PATH="/usr/local/opt/node@24/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/node@24/lib"
+# export CPPFLAGS="-I/usr/local/opt/node@24/include"
