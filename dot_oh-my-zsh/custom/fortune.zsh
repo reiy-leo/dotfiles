532569bc114e2be3785dd3af702e3cc5563886b3
RyanLincs <ryan.lincs@outlook.com>
2026-02-09 19:14:33 +0800

    关闭fortune

--- dot_oh-my-zsh/custom/fortune.zsh
+++ dot_oh-my-zsh/custom/fortune.zsh
@@ -3,7 +3,7 @@ function fortune_boxed() {
     fortune $HOME/.local/share/fortune/merged | boxes -d ansi --color | awk 'BEGIN { printf "%s", "\033[38;5;196m" } { printf "%s\n", $0 } END { printf "%s", "\033[0m" }'
 }
 
-autoload -Uz add-zsh-hook d
-add-zsh-hook precmd fortune_boxed
+# autoload -Uz add-zsh-hook d
+# add-zsh-hook precmd fortune_boxed
 
 # vim: set ft=zsh
