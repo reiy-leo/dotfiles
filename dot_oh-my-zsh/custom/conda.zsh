abda608e45793570a8d4bb0fdeb06e087f9cc0f0
RyanLincs <ryan.lincs@outlook.com>
2026-03-27 17:46:06 +0800

    配置conda

--- dot_oh-my-zsh/custom/conda.zsh
+++ dot_oh-my-zsh/custom/conda.zsh
@@ -1,14 +1,46 @@
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/apple/.local/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$("$HOME/.local/miniconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/Users/apple/.local/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/apple/.local/miniconda3/etc/profile.d/conda.sh"
+    if [ -f "$HOME/.local/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/.local/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/Users/apple/.local/miniconda3/bin:$PATH"
+        export PATH="$HOME/.local/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
-# <<< conda initialize <<<
+
+# 从omz plugin info conda中偷的
+# 不直接激活conda插件是因为和starship的prompt冲突了
+alias cna="conda activate"
+alias cnab="conda activate base"
+alias cnde="conda deactivate"
+alias cnel="conda env list"
+
+alias cnconf="conda config"
+alias cncss="conda config --show-source"
+
+alias cnle="conda list --export"
+alias cnles="conda list --explicit > spec-file.txt"
+
+alias cncf="conda env create -f"
+alias cncn="conda create -y -n"
+alias cncp="conda create -y -p"
+alias cncr="conda create -n"
+
+alias cni="conda install"
+alias cniy="conda install -y"
+alias cnl="conda list"
+alias cniy="conda install -y"
+alias cnl="conda list"
+
+alias cnr="conda remove"
+alias cnrn="conda remove -y -all -n"
+alias cnrp="conda remove -y -all -p"
+alias cnry="conda remove -y"
+
+alias cnsr="conda search"
+
+alias cnu="conda update"
+alias cnua="conda update --all"
+alias cnuc="conda update conda"
